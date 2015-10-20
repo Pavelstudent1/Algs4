4325bb48fe62162e5d539830100c360da037a673
unknown <pvfedorov6861@gmail.com>
2015-10-21 00:19:32 +0400

    some remarks in Brute from HW3

--- Algs4/src/homeWork3_Pattern_Recognition/BruteCollinearPoints.java
+++ Algs4/src/homeWork3_Pattern_Recognition/BruteCollinearPoints.java
@@ -23,16 +23,16 @@ public class BruteCollinearPoints {
 		Arrays.sort(tmp); //для брут-форса нужно отсортировать массив точек.
 						  //от точки с наименьшими координатами, до точки с
 						  //наибольшими
-		List<BilletLine> lines = new ArrayList<>();
+		List<BlankLine> lines = new ArrayList<>();
 
 		Point minPoint = null, maxPoint = null;
 
 		for (int i = 0; i < tmp.length; i++) {
 
 			if (maxPoint != null) {
-				BilletLine l = new BilletLine(minPoint, maxPoint); 
-				if (isDifferentLine(lines, l)) {
-					lines.add(l);
+				BlankLine bline = new BlankLine(minPoint, maxPoint); 
+				if (isDifferentLine(lines, bline)) {
+					lines.add(bline);
 				}
 //				lines.add(new BilletLine(minPoint, maxPoint));
 				maxPoint = null;
@@ -47,7 +47,12 @@ public class BruteCollinearPoints {
 								&& tmp[i].slopeTo(tmp[j]) == tmp[i].slopeTo(tmp[l])) {
 							// lines.add(new LineSegment(tmp[i], tmp[l]));
 							maxPoint = tmp[l];
-							//System.out.println(tmp[i] + " -> " + tmp[l]);
+							//как только нашли макс -> создаём линию
+							//в проверке линий нужно учесть, помимо того, что линия может помещаться в имеющуюся,
+							//также и то, что добавляемая может вместить в себя имеющуюся, тогда имеющаяся
+							//заменяется (перепись отличных координат) на добавляемую
+							System.out.print(tmp[i] + " -> " + tmp[l]);
+							System.out.printf("| i = %d, j = %d, k = %d, l = %d\n", i, j, k, l);
 						}
 
 					}
@@ -61,27 +66,38 @@ public class BruteCollinearPoints {
 		}
 
 	}
-	
-	private class BilletLine{
+	//вспомогательный класс с доступам к координатам, чего LineSegment не позволяет по условию задачи
+	private class BlankLine{
 		Point a = null;
 		Point b = null;
 		
-		public BilletLine(Point a, Point b) {
+		public BlankLine(Point a, Point b) {
 			this.a = a;
 			this.b = b;
 		}
 	}
 
-	private boolean isDifferentLine(List<BilletLine> lines, BilletLine l) {
+	
+	private boolean isDifferentLine(List<BlankLine> lines, BlankLine l) {
 		
 		if (lines.size() == 0) return true;
 		
-		for (BilletLine bl : lines) {
+		for (BlankLine bl : lines) { 
+//			if (isHorizontal(bl, l) && (bl.a.getY() != l.a.getY())) return true;
 			if (bl.a.compareTo(l.a) <= 0 && bl.b.compareTo(l.b) >= 0) return false;
 		}
 		
 		return true;
 	}
+	
+
+	private boolean isHorizontal(BlankLine bl, BlankLine l) {
+
+		if (bl.a.getY() == bl.b.getY() && l.a.getY() == l.b.getY()) return true;
+			
+		return false;
+	}
+
 
 	public int numberOfSegments() {
 		return linesegments.length;
