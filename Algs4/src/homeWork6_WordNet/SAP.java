fc53dfd9b43db073acb120b906b2131e02947764
Pavel_Fedorov <Pavel_Fedorov@epam.com>
2015-11-11 18:11:42 +0300

    HW6 update

--- Algs4/src/homeWork6_WordNet/SAP.java
+++ Algs4/src/homeWork6_WordNet/SAP.java
@@ -1,72 +1,137 @@
 package homeWork6_WordNet;
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 
-public class SAP { //SAP = Shortest Ancestral Path
+public class SAP { // SAP = Shortest Ancestral Path
 	private Digraph dg;
-	
+	private int min_length = -1;
+	private int common_ancestor = -1;
+	private int prev_v = -1;
+	private int prev_w = -1;
+
 	public SAP(Digraph G) {
-		if (G == null) throw new NullPointerException();
+		if (G == null)
+			throw new NullPointerException();
 		dg = G;
 	}
-	
-	public int length(int v, int w){
+
+	public int length(int v, int w) {
+
+		if (v < 0 || v > dg.V() - 1)
+			throw new IndexOutOfBoundsException();
+		if (w < 0 || w > dg.V() - 1)
+			throw new IndexOutOfBoundsException();
 		
-		return 0;
+		//т.к. наименьший путь и общий предок вычисляются одновременно,
+		//а запрашиваться путь и предок могут по отдельности,
+		//для ускорения храним эти данные, пока v и w не изменились
+		if (prev_v != v || prev_w != w) { 
+			calcLengthAndAncestor(v, w);
+		}
+
+		return this.min_length;
 	}
-	
-	public int ancestor(int v, int w){
-		
-		DirectedDFS dv = new DirectedDFS(dg, v);
-		DirectedDFS dw = new DirectedDFS(dg, w);
-		
-		int ancestor = -1, minlength = -1, i = 0;
-		
-		while(i < dg.V()){
-			if (dv.marked[i].wasHere == dw.marked[i].wasHere){
-				int length = dv.marked[i].length + dw.marked[i].length;
-				if (minlength == -1 || length > 0 && length <= minlength){
+
+	public int ancestor(int v, int w) {
+
+		if (v < 0 || v > dg.V() - 1)
+			throw new IndexOutOfBoundsException();
+		if (w < 0 || w > dg.V() - 1)
+			throw new IndexOutOfBoundsException();
+
+		if (prev_v != v || prev_w != w) {
+			calcLengthAndAncestor(v, w);
+		}
+
+		return this.common_ancestor;
+	}
+
+	private void calcLengthAndAncestor(int v, int w) {
+
+		BreadthFirstDirectedPaths V = new BreadthFirstDirectedPaths(dg, v);
+		BreadthFirstDirectedPaths W = new BreadthFirstDirectedPaths(dg, w);
+
+		int ancestor = -1, minlength = Integer.MAX_VALUE, i = 0;
+		//одновременно ищем предка и самый короткий путь до него из вершин v и w
+		while (i < dg.V()) {
+			if (V.hasPathTo(i) == W.hasPathTo(i)) {
+				int length = V.distTo(i) + W.distTo(i);
+				if (length > 0 && length <= minlength) {
 					ancestor = i;
 					minlength = length;
 				}
 			}
 			++i;
 		}
-		
-		
-		
-	
-		return ancestor;
+
+		this.common_ancestor = ancestor;
+		this.min_length = (minlength == Integer.MAX_VALUE ? -1 : minlength);
+		this.prev_v = v;
+		this.prev_w = w;
+
 	}
-	
-	public int length(Iterable<Integer> v, Iterable<Integer> w){
-		
+	////старый метод, работающий схожим образом с BreadthFirstDirectedPaths
+	////(старый метод == модифицированный поиск в глубину)
+	////правда не известно, который из них быстрее.
+	// private void calcLengthAndAncestor(int v, int w) {
+	// // Вычисляем путь от заданной вершины до "дна" графа методом поиска в
+	// глубину
+	// DirectedDFS dv = new DirectedDFS(dg, v);
+	// DirectedDFS dw = new DirectedDFS(dg, w);
+	//
+	// int ancestor = -1, minlength = Integer.MAX_VALUE, i = 0;
+	// // одновременно ищем предка и самый короткий путь до него из вершин v и w
+	// while (i < dg.V()) {
+	// if (dv.marked[i].wasHere == dw.marked[i].wasHere) {
+	// int length = dv.marked[i].length + dw.marked[i].length;
+	// if (length > 0 && length <= minlength) {
+	// ancestor = i;
+	// minlength = length;
+	// }
+	// }
+	// ++i;
+	// }
+	//
+	// this.common_ancestor = ancestor;
+	// this.min_length = (minlength == Integer.MAX_VALUE ? -1 : minlength);
+	// this.prev_v = v;
+	// this.prev_w = w;
+	// }
+
+	public int length(Iterable<Integer> v, Iterable<Integer> w) {
+
 		return 0;
 	}
-	
-	public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
-		
+
+	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+
 		return 0;
 	}
-	
-	public static void main(String[] args) {
-		
-	
-	}
-	
+
 	private class DirectedDFS {
-		private class Node{
+
+		private class Node {
 			boolean wasHere;
 			int length;
-			
+
 			public Node() {
 				wasHere = false;
 				length = -1;
 			}
+
+			public void set(int length) {
+				wasHere = true;
+				this.length = length;
+			}
+
 		}
-		
+
 		Node[] marked;
-		
+
 		public DirectedDFS(Digraph G, int s) {
 			marked = new Node[G.V()];
 			for (int i = 0; i < marked.length; i++) {
@@ -76,13 +141,31 @@ public class SAP { //SAP = Shortest Ancestral Path
 		}
 
 		private void dfs(Digraph G, int v, int len) {
-			marked[v].wasHere = true;
-			marked[v].length = len;
-		
+			marked[v].set(len);
+
 			for (int w : G.adj(v)) {
-				if (!marked[w].wasHere) dfs(G, w, ++len);
+				if (marked[w].wasHere) { // если в данной вершине уже были
+					if (marked[w].length > len) // и если новый путь до неё
+												// короче предыдущего
+						marked[w].length = len; // переписываем имеющийся
+				} else
+					dfs(G, w, ++len);
 			}
 		}
-		
+
+	}
+
+	public static void main(String[] args) {
+
+		In in = new In(args[0]);
+		Digraph G = new Digraph(in);
+		SAP sap = new SAP(G);
+		while (!StdIn.isEmpty()) {
+			int v = StdIn.readInt();
+			int w = StdIn.readInt();
+			int length = sap.length(v, w);
+			int ancestor = sap.ancestor(v, w);
+			StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+		}
 	}
 }
--- Algs4/src/homeWork6_WordNet/WordNet.java
+++ Algs4/src/homeWork6_WordNet/WordNet.java
@@ -1,11 +1,107 @@
 package homeWork6_WordNet;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
 public class WordNet {
+	
+	private Digraph DG;
+	private int dgLength = 0;
+	
+	private Map<Integer, Integer[]> verticeWithHyp = new HashMap<>();
+	private Map<Integer, String[]> synsets = new HashMap<>();
+	private Map<String, ArrayList<Integer>> all_nouns = new HashMap<>();
+	
 	
 	public WordNet(String synset, String hypernyms) {
 		if (synset == null || hypernyms == null) throw new NullPointerException();
+		collectSynsets(synset);
+		collectHypernyms(hypernyms);
+		
+		this.DG = new Digraph(dgLength);
+		
+		for (Map.Entry<Integer, Integer[]> vert : verticeWithHyp.entrySet()) {
+			for (Integer el : vert.getValue()) {
+				this.DG.addEdge(vert.getKey(), el);
+			}
+		}
+		//https://github.com/destructivecreator/coursera/blob/master/wordnet/src/WordNet.java
+		
+		System.out.println("Done!");
+		
+		
 	}
 	
+	private void collectSynsets(String synset) {
+		
+		In in = new In(synset);
+		String line = null;
+		String[] splittedline = null;
+		String[] synonyms = null;
+		int synonymsID = 0;
+		ArrayList<Integer> idConnectedToNoun = null;
+		
+		
+		while(!in.isEmpty()) {
+			line = in.readLine();
+			splittedline = line.split(",");
+			synonymsID = Integer.parseInt(splittedline[0]);
+			synonyms = splittedline[1].split(" ");
+			
+			for (String s : synonyms) {
+				
+				idConnectedToNoun = all_nouns.get(s);
+				if (idConnectedToNoun != null){
+					System.out.println("Match noun: " + s);
+					idConnectedToNoun.add(synonymsID);
+				}else{
+					idConnectedToNoun = new ArrayList<>();
+					idConnectedToNoun.add(synonymsID);
+					all_nouns.put(s, idConnectedToNoun);
+				}
+			}
+			
+			this.synsets.put(synonymsID, synonyms);
+			
+
+			++this.dgLength;
+		}
+		
+		System.out.println("Done!");
+		
+	}
+
+	private void collectHypernyms(String hypernyms) {
+
+		In in = new In(hypernyms);
+		String line = null;
+		String[] splittedline = null;
+		Integer[] arrayOfHyps;
+		
+		while(!in.isEmpty()){
+			
+			line = in.readLine();
+			splittedline = line.split(",");
+			arrayOfHyps = new Integer[splittedline.length - 1];
+			
+			for (int i = 0; i < arrayOfHyps.length; i++) {
+				arrayOfHyps[i] = Integer.valueOf(splittedline[i + 1]);
+			}
+			
+			verticeWithHyp.put(Integer.valueOf(splittedline[0]), arrayOfHyps);
+			
+
+		}
+		
+		System.out.println("Done!");
+		
+	}
+
 	public Iterable<String> nouns(){
 		return null;
 	}
@@ -31,6 +127,7 @@ public class WordNet {
 	
 	public static void main(String[] args) {
 		
+		WordNet wn = new WordNet(args[0], args[1]);
 	}
 	
 }
